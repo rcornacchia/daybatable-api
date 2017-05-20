9cac8f4b1632671566edf6d9d5c2ad40a1060b26
rcornacchia <rlc2160@columbia.edu>
2017-05-20 15:58:33 -0400

    added authenticate route

--- server.js
+++ server.js
@@ -18,10 +18,67 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.post('/authenticate', (req, res) => {
-  console.log(req.body);
-  res.send('Hello ' + req.body.username);
+app.get('/setup', (req, res) => {
+  const rob = new User({
+    username: 'robco',
+    password: 'changemefortheloveofgod',
+    admin: true,
+    email: 'bobbycornacchia@gmail.com',
+    firstName: 'Rob',
+    lastName: 'Cornacchia',
+  });
+
+  rob.save(err => {
+    if (err) throw err;
+    console.log('User saved successfully');
+    res.json({ success: true });
+  })
 });
 
+const apiRoutes = express.Router();
+
+// route to authenticate a user
+apiRoutes.post('/authenticate', (req, res) => {
+  User.findOne({
+    username: req.body.username
+  }, (err, user) => {
+    if (err) throw err;
+    if (!user) {
+      res.json({
+        success: false,
+        message: 'Incorrect username and password combination.'
+      });
+    } else {
+      if (user.password != req.body.password) {
+        res.json({
+          success: false,
+          message: 'Incorrect username and password combination'
+        });
+      } else {
+        const token = jwt.sign(user, app.get('secret'), {
+          expiresIn: 1440
+        });
+
+        res.json({
+          success: true,
+          message: 'Authentication successful',
+          token,
+          user
+        });
+      }
+    }
+  });
+});
+
+// route middleware to verify a token
+
+// route to return all users
+apiRoutes.get('/users', (req, res) => {
+  User.find({}, (err, users) => {
+    res.json(users);
+  });
+});
+
+app.use('/api', apiRoutes);
 app.listen(port);
 console.log(`Server started on port: ${port}`);
